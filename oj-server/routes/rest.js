72664039f01a8cf683ad405b096036e6eb93c35e
weilun <weilun.cheng@stonybrook.edu>
2019-03-17 14:23:29 -0700

    finish v0.0.1

--- oj-server/routes/rest.js
+++ oj-server/routes/rest.js
@@ -4,6 +4,13 @@ var problemService = require("../services/problemService");
 var bodyParser = require("body-parser");
 var jsonPaser = bodyParser.json();
 
+var node_rest_client = require('node-rest-client').Client;
+var rest_client = new node_rest_client();
+
+EXECUTOR_SERVER_URL = 'http://127.0.0.1:5000/build_and_run';
+
+rest_client.registerMethod('build_and_run',EXECUTOR_SERVER_URL, 'POST');
+
 router.get("/problems", function (req,res) {
     console.log("get problems");
     problemService.getProblems()
@@ -25,6 +32,26 @@ router.post("/problems", jsonPaser, function (req,res) {
             }, function (error) {
             res.status(400).send("Problem name already exists!");
         });
+});
+
+router.post("/build_and_run", jsonPaser, function (req, res) {
+    const userCode = req.body.user_code;
+    const lang = req.body.lang;
+
+    console.log(lang+':'+userCode);
+    rest_client.methods.build_and_run(
+        {
+            data:{code: userCode, lang: lang},
+            headers: { "Content-Type": "application/json"}
+        }, (data,response) =>{
+            console.log("Received response from execution server: " + response);
+            const text = `Build output: ${data['build']}
+      Execute output: ${data['run']}`;
+
+            data['text'] = text;
+            res.json(data);
+        }
+    )
 });
 
 module.exports = router;
